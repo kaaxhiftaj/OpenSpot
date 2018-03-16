8f532ad868d4eb528217966b5d3978798ba7abca
Kashif Taj <kaxhiftaj@gmail.com>
2018-03-16 10:49:51 +0500

    Initial commit

---
+++ app/src/main/java/com/techease/openspot/ui/activities/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.techease.openspot.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.techease.openspot.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        
+    }
+}
---
+++ app/src/main/java/com/techease/openspot/utils/Configuration.java
@@ -0,0 +1,12 @@
+package com.techease.openspot.utils;
+
+/**
+ * Created by Asus on 9/18/2017.
+ */
+
+public class Configuration {
+        public static String USER_URL = "http://openspot.com/";
+    public static String MY_PREF = "com.openspot";
+
+
+}
